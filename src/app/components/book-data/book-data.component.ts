15583ef16a4299d70971e52fe68c921e65028a67
kamilac11 <kamilachoja@gmail.com>
2019-10-20 23:17:33 +0200

    get list of chosen books position, not working function with DOM elemnt...

--- src/app/components/book-data/book-data.component.ts
+++ src/app/components/book-data/book-data.component.ts
@@ -21,6 +21,7 @@ export class BookDataComponent implements OnInit {
 
   //dataSource = bookService.
   //books = new MatTableDataSourceource();
+  choosenBooks = new Array(); 
   bookData: Book[] = new Array();
   bookDataTest: Book[] = new Array();
   tekst: string = "notebook.png"
@@ -37,11 +38,9 @@ export class BookDataComponent implements OnInit {
       this.bookService.getBooksByCover("notebook.png").subscribe(val2 => {this.bookData=val2;
         console.log(val2);
       });
+
+     
 }
-
-
-
-  
 
 //lbl to obiekt np label, z kolei val to wartosc - name obiektu ktora zostala mu dana, parametr lbl musi byc bo inacej nie poda nazwy tylko id
   getNameChosenPicture(lbl, val){
@@ -62,10 +61,77 @@ export class BookDataComponent implements OnInit {
   testClick(){
      console.log(this.bookList);
     return this.bookList.toString();
-   
-    
   }
 
+  
+addBookToArray() {
+//switch?
+
+
+
+  if(document.getElementById('button1').onclick && this.choosenBooks.includes('img1') == false ){
+    this.choosenBooks.push('img1');
+  }
+  else if (document.getElementById('button2').onclick && this.choosenBooks.includes('img2') == false){
+    this.choosenBooks.push('img2');
+  }
+  else if(document.getElementById('button3').onclick && this.choosenBooks.includes('img3') == false){
+      this.choosenBooks.push('img3');
+  }
+  else if(document.getElementById('button4').onclick && this.choosenBooks.includes('img4') == false){
+    this.choosenBooks.push('img4');
+  }
+
+  document.getElementById('button1').onclick = function () {
+    // document.getElementById('button1')
+     
+  } 
+  
+  document.getElementById('button2').onclick = function () {
+    document.getElementById('button2')
+  } 
+
+  
+  // document.getElementById('button1').onclick = this.checkImg1;
+  // document.getElementById('button2').onclick = this.checkImg2;
+  // document.getElementById('button3').onclick = this.checkImg3;
+  // document.getElementById('button4').onclick = this.checkImg4;
+
+
+
+  console.log(this.choosenBooks);
+
+}
+
+checkImg1() {
+  if(this.choosenBooks.includes('img1')==false){
+    this.choosenBooks.push('img1');
+  }
+}
+
+
+checkImg2() {
+  if(this.choosenBooks.includes('img2')==false){
+    this.choosenBooks.push('img2');
+  }
+  console.log(this.choosenBooks);
+}
+
+
+checkImg3() {
+  if(this.choosenBooks.includes('img3')==false){
+    this.choosenBooks.push('img3');
+  }
+  console.log(this.choosenBooks);
+}
+
+
+checkImg4() {
+  if(this.choosenBooks.includes('img4')==false){
+    this.choosenBooks.push('img4');
+  }
+  console.log(this.choosenBooks);
+}
 
 
 }
